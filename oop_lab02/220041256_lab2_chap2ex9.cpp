17f9e85138600cf441c201fe8b37b971c48b6e6c
aronno256 <tahsinahamed@iut-dhaka.edu>
2024-09-21 00:50:56 +0600

    message

---
+++ oop_lab02/220041256_lab2_chap2ex9.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c,d,r1,r2;
+    char ch;
+    cin>>a>>ch>>b;
+    cin>>c>>ch>>d;
+    r1=(a*d+b*c);
+    r2=b*d;
+    cout<<r1<<ch<<r2<<endl;
+
+}
